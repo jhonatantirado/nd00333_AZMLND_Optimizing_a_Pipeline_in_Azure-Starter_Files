f8c0c84e2f6845d93d96585dba8befca12e37d7f
jhonatantirado <jhonatantiradotiradodeep@gmail.com>
2020-11-22 19:19:01 -0500

    Added joblib fix

--- train.py
+++ train.py
@@ -72,6 +72,8 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    os.makedirs('./outputs', exist_ok=True)
+    joblib.dump(value=model, './outputs/hd-model.joblib')
 
 if __name__ == '__main__':
     main()
